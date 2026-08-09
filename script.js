0db85dd8dae4214ad732a603d1950be67e77f29e
Achut Poudel <poudel.achut@gmail.com>
2026-08-09 14:38:11 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,93 @@
-/* =========================================
+/* =====================================================
+   MOBILE MENU
+===================================================== */
+
+const mobileMenuBtn =
+    document.getElementById("mobileMenuBtn");
+
+const mobileMenu =
+    document.getElementById("mobileMenu");
+
+
+if (mobileMenuBtn && mobileMenu) {
+
+
+    mobileMenuBtn.addEventListener(
+        "click",
+        function () {
+
+            mobileMenu.classList.toggle("active");
+
+
+            const icon =
+                mobileMenuBtn.querySelector("i");
+
+
+            if (
+                mobileMenu.classList.contains("active")
+            ) {
+
+                icon.classList.remove(
+                    "fa-bars"
+                );
+
+                icon.classList.add(
+                    "fa-xmark"
+                );
+
+            } else {
+
+                icon.classList.remove(
+                    "fa-xmark"
+                );
+
+                icon.classList.add(
+                    "fa-bars"
+                );
+
+            }
+
+        }
+    );
+
+
+    /* Close menu after clicking */
+
+    const mobileLinks =
+        mobileMenu.querySelectorAll("a");
+
+
+    mobileLinks.forEach(
+        function (link) {
+
+            link.addEventListener(
+                "click",
+                function () {
+
+                    mobileMenu.classList.remove(
+                        "active"
+                    );
+
+
+                    const icon =
+                        mobileMenuBtn.querySelector("i");
+
+
+                    icon.classList.remove(
+                        "fa-xmark"
+                    );
+
+                    icon.classList.add(
+                        "fa-bars"
+                    );
+
+                }
+            );
+
+        }
+    );
+
+}/* =========================================
    MOBILE NAVIGATION
 ========================================= */
 
