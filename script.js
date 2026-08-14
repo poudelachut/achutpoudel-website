ade565d022d2021b740f1d5ec1867a05b27517cc
Achut Poudel <poudel.achut@gmail.com>
2026-08-14 13:21:12 +0545

    Update script.js

--- script.js
+++ script.js
@@ -121,3 +121,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 });
+
+
+/* =========================================================
+   WEBSITE SCRIPT
+   ========================================================= */
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const yearElement = document.getElementById("currentYear");
+
+    if (yearElement) {
+        yearElement.textContent = new Date().getFullYear();
+    }
+
+});
